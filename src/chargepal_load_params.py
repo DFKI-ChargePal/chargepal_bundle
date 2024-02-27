db01811a4a805ad5a6e9768ad44fec404c273118
gparthasarathy <gurunatraj.parthasarathy@dfki.de>
2024-02-27 12:41:24 +0100

    adding confg parameters and delay in launching nodes
    Signed-off-by: gparthasarathy <gurunatraj.parthasarathy@dfki.de>

--- src/chargepal_load_params.py
+++ src/chargepal_load_params.py
@@ -6,8 +6,10 @@ import rospkg
 import yaml
 
 rospack = rospkg.RosPack()
+
+
 def load_yaml_params(file_path):
-    with open(file_path, 'r') as stream:
+    with open(file_path, "r") as stream:
         try:
             params = yaml.safe_load(stream)
             return params
@@ -15,14 +17,16 @@ def load_yaml_params(file_path):
             rospy.logerr(f"Error loading config.yaml file: {exc}")
             return None
 
+
 def set_ros_params(params):
     if params:
         for key, value in params.items():
             rospy.set_param(key, value)
 
-if __name__ == '__main__':
-    rospy.init_node('chargepal_load_params_node')
-    config_file_path = rospack.get_path("chargepal_bundle")+"/cfg/config.yaml"
+
+if __name__ == "__main__":
+    rospy.init_node("chargepal_load_params_node")
+    config_file_path = rospack.get_path("chargepal_bundle") + "/cfg/config.yaml"
     config_params = load_yaml_params(config_file_path)
     set_ros_params(config_params)
     rospy.spin()
--- src/chargepal_startup.py
+++ src/chargepal_startup.py
@@ -3,18 +3,19 @@
 import rospy
 import sys
 import rospkg
+import os
 
 rospack = rospkg.RosPack()
-sys.path.insert(0,rospack.get_path("chargepal_client")+"/src/chargepal_client")
+sys.path.insert(0, rospack.get_path("chargepal_client") + "/src/chargepal_client")
 
 from chargepal_client.client import Grpc_Client
+
 
 def pull_ldb_to_rdb_rdb_copy():
     client_instance = Grpc_Client()
     client_instance.pull_ldb()
 
 
-if __name__ == '__main__':
-    rospy.init_node('chargepal_startup_node')
+if __name__ == "__main__":
+    rospy.init_node("chargepal_startup_node")
     pull_ldb_to_rdb_rdb_copy()
-    rospy.spin()
