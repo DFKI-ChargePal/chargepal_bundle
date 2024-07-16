cd0527774878535f1007344fc8fcb94c2cac2165
gparthasarathy <gurunatraj.parthasarathy@dfki.de>
2024-07-16 12:30:20 +0200

    removes chargepal_monitor_gui dependencies
    Signed-off-by: gparthasarathy <gurunatraj.parthasarathy@dfki.de>

--- src/chargepal_load_params.py
+++ src/chargepal_load_params.py
@@ -7,9 +7,6 @@ import yaml
 
 rospack = rospkg.RosPack()
 
-gui_yaml_path = rospack.get_path("chargepal_monitor_gui") + "/cfg/gui.yaml"
-
-
 def load_yaml_params(file_path):
     with open(file_path, "r") as stream:
         try:
@@ -32,35 +29,9 @@ def set_ros_params(params):
             rospy.set_param(key, value)
 
 
-def set_gui_params(updated_values):
-
-    with open(gui_yaml_path, "r") as file:
-        data = yaml.safe_load(file)
-
-    # Update the specific values
-    for key, value in updated_values.items():
-        data[key] = value
-
-    with open(gui_yaml_path, "w") as file:
-        yaml.dump(data, file)
-
-
 if __name__ == "__main__":
     rospy.init_node("chargepal_load_params_node")
     config_file_path = rospack.get_path("chargepal_bundle") + "/cfg/config.yaml"
     config_params = load_yaml_params(config_file_path)
     set_ros_params(config_params)
-
-    updated_values = {
-        "ongoing_action": "",
-        "ongoing_job": "",
-        "gui_demo_resume": False,
-        "gui_demo_start": False,
-        "gui_demo_stop": True,
-        "gui_log": "",
-        "gui_loop_input": 0,
-        "robot_current_loop": 0,
-    }
-
-    set_gui_params(updated_values)
     rospy.spin()
